8046185c656b49daf35109dc8ba9b5998ea8afde
Taylor Pridgen <tpridgen@starrez.com>
2020-08-12 21:00:44 -0600

    master/ secret

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import faunadb, { query as q } from "faunadb";
 
 function App() {
+  var client = new faunadb.Client({
+    secret: process.env.fauna_secret || "",
+  });
+
+  client
+    .query(q.Get(q.Ref(q.Collection("gametable"), "273705832001045004")))
+    .then((ret: any) => console.log(ret?.data?.test))
+    .catch((issue) => console.log("uhoh", issue));
+
   return (
     <div className="App">
       <header className="App-header">
