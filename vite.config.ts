59488c13f5e9e25aabb71661d2e2aa8c21f90d96
Taylor Pridgen <musicinsilence13@gmail.com>
2023-02-26 02:17:41 -0700

    solid/ vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import vercel from "solid-start-vercel";
 
 export default defineConfig({
   plugins: [
     solid({
+      adapter: vercel(),
       babel: (_, id) => ({
         plugins: [["solid-styled/babel", { source: id }]],
       }),
