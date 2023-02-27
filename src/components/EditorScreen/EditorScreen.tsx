e14dd080a685af9cc7ffdb85b75a28792c043a61
Taylor Pridgen <musicinsilence13@gmail.com>
2023-02-26 20:37:50 -0700

    solid/ man I love when shit just works

--- src/components/EditorScreen/EditorScreen.tsx
+++ src/components/EditorScreen/EditorScreen.tsx
@@ -1,5 +1,6 @@
-import { For } from "solid-js";
+import { For, onCleanup } from "solid-js";
 import { css } from "solid-styled";
+import { calculatePosition } from "~/utilities";
 
 export const EditorScreen = () => {
   css`
@@ -23,86 +24,55 @@ export const EditorScreen = () => {
 
   const items = [1, 2];
 
+  const handleMouseUp = () => {
+    // Find any elements with .draggable
+    const draggableItems = document.getElementsByClassName("dragging");
+    // Remove all attributes and class from draggable elements
+    for (const item of draggableItems) {
+      item.classList.remove("dragging");
+      item.removeAttribute("data-start-x");
+      item.removeAttribute("data-start-y");
+      item.removeAttribute("data-element-x");
+      item.removeAttribute("data-element-y");
+    }
+
+    // Remove move events
+    document.documentElement.removeEventListener("mousemove", handleMouseMove);
+  };
+
+  const handleMouseMove = (e: MouseEvent) => {
+    // Grab elements for calculations
+    const itemTarget: HTMLElement = document.getElementsByClassName(
+      "dragging"
+    )?.[0] as HTMLElement;
+    const container: HTMLElement = document.getElementById(
+      "editor-screen"
+    ) as HTMLElement;
+
+    calculatePosition(e, itemTarget, container);
+  };
+
+  onCleanup(() => {
+    document.documentElement.removeEventListener("mouseup", handleMouseUp);
+    document.documentElement.removeEventListener("mousemove", handleMouseMove);
+  });
+
   return (
-    <div
-      class="screen"
-      id="editor-screen"
-      onmousemove={(e) => {
-        const itemTarget: HTMLElement = document.getElementsByClassName(
-          "dragging"
-        )?.[0] as HTMLElement;
-
-        /* Calculates the distance the mouse has moved
-          and applies that amount to the element's initial position
-
-          Also respects the current bounds of the container
-        */
-        if (itemTarget?.classList.contains("dragging")) {
-          // Element's initial position
-          const elementInitialX =
-            Number(itemTarget.getAttribute("data-element-x")) ?? 0;
-          const elementInitialY =
-            Number(itemTarget.getAttribute("data-element-y")) ?? 0;
-
-          // Get how much mouse has moved
-          const mouseOffsetX =
-            e.clientX - Number(itemTarget.getAttribute("data-start-x"));
-          const mouseOffsetY =
-            e.clientY - Number(itemTarget.getAttribute("data-start-y"));
-
-          // Gets windows maximum bounds
-          const screen = document
-            .getElementById("editor-screen")
-            ?.getBoundingClientRect();
-          const itemBox = itemTarget.getBoundingClientRect();
-          const screenMaxX =
-            (screen?.x ?? 0) + (screen?.width ?? 0) + (itemBox.width ?? 0);
-          const screenMaxY =
-            (screen?.y ?? 0) + (screen?.height ?? 0) + (itemBox.height ?? 0);
-
-          // Move element to same distance the mouse has moved
-          // might need to do some checks here that the new position isn't outside the container's boundaries
-          itemTarget.style.left = `${Math.min(
-            screenMaxX,
-            Math.max(0, elementInitialX + mouseOffsetX)
-          )}px`;
-          itemTarget.style.top = `${Math.min(
-            screenMaxY,
-            Math.max(0, elementInitialY + mouseOffsetY)
-          )}px`;
-        }
-      }}
-      onmouseup={(e) => {
-        for (const item of e.currentTarget.children) {
-          item.classList.remove("dragging");
-          item.removeAttribute("data-start-x");
-          item.removeAttribute("data-start-y");
-          item.removeAttribute("data-element-x");
-          item.removeAttribute("data-element-y");
-        }
-      }}
-      onmouseleave={(e) => {
-        for (const item of e.currentTarget.children) {
-          item.classList.remove("dragging");
-          item.removeAttribute("data-start-x");
-          item.removeAttribute("data-start-y");
-          item.removeAttribute("data-element-x");
-          item.removeAttribute("data-element-y");
-        }
-      }}
-    >
+    <div class="screen" id="editor-screen">
       <For each={items}>
         {(item, index) => (
           <div
             style={{ background: `hsl(${Math.random() * 360}, 50%, 50%)` }}
             class="item"
             onmousedown={(e) => {
+              // Grab element being clicked
               const itemTarget = e.currentTarget;
               // Mark item as being dragged
               itemTarget.classList.add("dragging");
               // Set initial mouse position
               itemTarget.setAttribute("data-start-x", e.clientX.toString());
               itemTarget.setAttribute("data-start-y", e.clientY.toString());
+              // Set initial element position
               itemTarget.setAttribute(
                 "data-element-x",
                 itemTarget.style.left.replace("px", "") ?? 0
@@ -111,14 +81,16 @@ export const EditorScreen = () => {
                 "data-element-y",
                 itemTarget.style.top.replace("px", "") ?? 0
               );
-            }}
-            onmouseup={(e) => {
-              const itemTarget = e.currentTarget;
-              itemTarget.classList.remove("dragging");
-              itemTarget.removeAttribute("data-start-x");
-              itemTarget.removeAttribute("data-start-y");
-              itemTarget.removeAttribute("data-element-x");
-              itemTarget.removeAttribute("data-element-y");
+              // Add listener to document so we remove data and classes no matter where user stops clicking
+              document.documentElement.addEventListener(
+                "mouseup",
+                handleMouseUp
+              );
+              // Add listener to document so we move the position no matter where the mouse is
+              document.documentElement.addEventListener(
+                "mousemove",
+                handleMouseMove
+              );
             }}
           >
             {item}
